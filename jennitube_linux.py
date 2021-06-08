444faf532148b43f30df060f2235180613f329c4
Arnaldo Carlos Lucas <arnaldo.carlos.lucas@gmail.com>
2021-06-07 23:38:12 -0300

    Creating .py versions for linux and windows.

--- jennitube_linux.py
+++ jennitube_linux.py
@@ -2,30 +2,36 @@ from logging import NullHandler
 from pytube import YouTube
 from pytube import Playlist
 import os
-from tkinter.filedialog import askdirectory
 
-def music(path):
-    link = str(input("Enter the link of the music you want: "))
+def music():
+    print()
+    link = str(input("Enter the song link on youtube: "))
+    print()
 
     counter = 0
     should_not_pass = False
     while should_not_pass == False:
         try:
+            print("Downloading...", '\n')
             yt = YouTube(link).streams.filter(only_audio=True,file_extension='mp4')
             yt2 = YouTube(link).title
             for i in yt:
-                i.download(path)
+                i.download()
+                print(90*'#')
                 print(f"{yt2} successfully downloaded!")
-            should_not_pass = True
+                print(90*'#', '\n')
+                should_not_pass = True
         except:
             print(f'Error 404, Trying again... COUNTER={counter}/10')
             counter+=1
         if counter == 10:
-                print("Download fail :(")
+                print("Download fail :(", '\n')
                 should_not_pass = True                       
 
-def playlist(path):        
-    link = str(input("Enter the link of the playlist you want: "))
+def playlist():        
+    print()
+    link = str(input("Enter the playlist link on youtube: "))
+    print()
 
     should_not_pass = False
     while should_not_pass == False:
@@ -33,37 +39,34 @@ def playlist(path):
             yt = Playlist(link)
             should_not_pass = True
         except:
-            print("Error 404, Trying again...")
+            print("Error 404, Trying again...", '\n')
             
     for video in yt.videos:
         should_not_pass = False
         counter = 0
         while should_not_pass == False:
             try:
+                print("Downloading...")             
                 yt = video.streams.filter(only_audio=True, file_extension='mp4')
-                yt.first().download(path)
+                yt.first().download()
                 yt2 = video.title
-                print(f"{yt2} successfully downloaded!")
+                print(f"{yt2} successfully downloaded!", '\n')
                 should_not_pass = True
             except:
                 print(f'Error 404, Trying again... COUNTER={counter}/10')  
                 counter+=1  
             if counter == 10:
-                print("Download fail :(")
+                print("Download fail :(", '\n')
                 should_not_pass = True
 
 if __name__=='__main__':
     jenni = 0
     while jenni != '1' or jenni != '2':
-        jenni = input("Do you want to download a single music or an entire playlist? \n1 - Single music\n2 - Playlist\n3 - Exit program\n")
+        jenni = input("\nDo you want to download a single music or an entire playlist? (Choose and press Enter) \n1 - Single music\n2 - Playlist\n3 - Exit program\n\n")
         if jenni in ('1','2'):         
-            print('Select a folder to save your music(s): ')   
-            path = askdirectory()
-            if path == "":
-                exit(0)
             if jenni == '1':
-                music(path)
+                music()
             if jenni == '2':
-                playlist(path)
+                playlist()
         if jenni == '3':
             exit(0)
